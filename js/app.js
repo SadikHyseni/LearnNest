04cc22e003f1a03d173db24b08521947581ca868
George Sadik Hyseni <122787525+SadikHyseni@users.noreply.github.com>
2024-11-04 16:04:59 +0000

    From Workshop 1: 1) Set up the basic HTML structure, created a HTML file with a placeholder <div id='app\'> where Vue mounts and linked to the Vue.js library in the HTML and set up a script to handle Vue JavaScript. 2) Initialized a basic Vue instance using el to mount it to the div with id=app in the HTML; also added a data property with a simple object containing the application name and a sample lesson object with attributes like title, description, location, price, and image. 3) Displayed data with interpolation {{ }} to display the lesson.

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        appName: "LearnNest",
+        lesson: {
+            title: "Introduction to Robotics",
+            location: "Heldon",
+            description: "Learn the basics of robotics in a fun and engaging environment.",
+            price: 50,
+            image: "images/computer_science.png",
+
+        }
+    }
+});
